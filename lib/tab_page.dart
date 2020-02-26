7b4d609f8294d291c24a5728fe48a041eff0185c
SeonHyungJo <seonhyung.jo@gmail.com>
2020-02-27 01:00:05 +0900

    :iphone: flutter instagram clone init

---
+++ lib/tab_page.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/account_page.dart';
+import 'package:instagram_clone/home_page.dart';
+import 'package:instagram_clone/search_page.dart';
+
+class TabPage extends StatefulWidget {
+  final FirebaseUser user;
+ 
+  TabPage(this.user);
+
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIdx = 0;
+  List _pages = [
+    HomePage(),
+    SearchPage(),
+    Accountpage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pages[_selectedIdx],),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIdx,
+        items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Sccount')),
+      ]),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIdx = index;
+    });
+  }
+}
